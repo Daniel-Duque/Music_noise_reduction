a2d075f23bf9a253a0488508b273be7cf4b9bd7b
Daniel-Duque <dduque80@hotmail.com>
2023-04-18 21:19:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ import tensorflow as tf
 import os
 import tensorflow_io as tfio
 from IPython.display import Audio
+
 #preprocess functions
 
 
 
 general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet/train_data"
 
-for i in os.listdir(general_path):
-    particular_path=os.path.join(general_path,i)
-    audio=tfio.audio.AudioIOTensor(particular_path)
-    audio_slice = audio[100:]
-    
-    # remove last dimension
-    audio_tensor = tf.squeeze(audio_slice, axis=[-1])
-    
+def audio_dataset(general_path):
+    n=0
+    for i in os.listdir(general_path):
+        n+=1
+        particular_path=os.path.join(general_path,i)
+            
+        audio = tfio.audio.AudioIOTensor(particular_path)
+        audio_slice = audio[:]
 
+        # remove last dimension
+        audio_tensor = tf.squeeze(audio_slice)
         
+        print(audio_tensor.numpy())
+        print(i)
+        rate=audio.rate.numpy()
+        
+        
+audio_dataset(general_path)
