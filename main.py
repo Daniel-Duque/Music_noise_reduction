e9e6d0ccfb39b676681298b915c3724cb194ed84
Daniel-Duque <dduque80@hotmail.com>
2023-04-22 21:13:31 -0500

    first running pilot

--- main.py
+++ main.py
@@ -3,34 +3,36 @@ import tensorflow as tf
 import os
 import tensorflow_io as tfio
 from IPython.display import Audio
-
+from scipy.io.wavfile import read
+import numpy as np
 #preprocess functions
 
 
 
-general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet"
+general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet/train_data"
 time=16000
+sizes=1000
 
 
-dataset=tf.keras.utils.audio_dataset_from_directory(
-    general_path,
-    labels='inferred',
-    label_mode='int',
-    class_names=None,
-    batch_size=32,
-    sampling_rate=None,
-    output_sequence_length=time,
-    ragged=False,
-    shuffle=True,
-    seed=None,
-    validation_split=None,
-    subset=None,
-    follow_links=False
-)
+
+#TODO make this able to receive multiple sizes
+dataset=np.empty((sizes))
+for i in os.listdir(general_path):
+    part_path=os.path.join(general_path,i)
+    song = read(part_path)
+    npsong=np.array(song[1],dtype=float)
+    padding=sizes-len(npsong)
+    if padding>0:
+        padsong=np.pad(npsong, (0,padding), 'constant', constant_values=(0,0))
+    else:
+        padsong=npsong[:sizes]
+    mean=-2.74658203125e-07
+    padsongnorm=padsong/mean
+    dataset=np.vstack((dataset,padsongnorm))
 
 model_rn = tf.keras.Sequential()
-model_rn.add(tf.keras.layers.LSTM(8, input_shape=(2,5,)))
+model_rn.add(tf.keras.layers.LSTM(8, input_shape=(sizes,1,), return_sequences=True))
+model_rn.add(tf.keras.layers.LSTM(1, return_sequences=True))
 
-
-model_rn.compile(loss=)
-model_rn.fit(dataset)
+model_rn.compile(loss="MSE")
+model_rn.fit(dataset,dataset,epochs=1000)
