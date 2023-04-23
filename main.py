c460933a1d2bd22ba524150f3199d3f8e86ea3f3
Daniel-Duque <dduque80@hotmail.com>
2023-04-22 20:19:58 -0500

    some starting trials for the dataset loading and an hipersimplistic model

--- main.py
+++ main.py
@@ -8,23 +8,29 @@ from IPython.display import Audio
 
 
 
-general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet/train_data"
+general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet"
+time=16000
 
-def audio_dataset(general_path):
-    n=0
-    for i in os.listdir(general_path):
-        n+=1
-        particular_path=os.path.join(general_path,i)
-            
-        audio = tfio.audio.AudioIOTensor(particular_path)
-        audio_slice = audio[:]
 
-        # remove last dimension
-        audio_tensor = tf.squeeze(audio_slice)
-        
-        print(audio_tensor.numpy())
-        print(i)
-        rate=audio.rate.numpy()
-        
-        
-audio_dataset(general_path)
+dataset=tf.keras.utils.audio_dataset_from_directory(
+    general_path,
+    labels='inferred',
+    label_mode='int',
+    class_names=None,
+    batch_size=32,
+    sampling_rate=None,
+    output_sequence_length=time,
+    ragged=False,
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    follow_links=False
+)
+
+model_rn = tf.keras.Sequential()
+model_rn.add(tf.keras.layers.LSTM(8, input_shape=(2,5,)))
+
+
+model_rn.compile(loss=)
+model_rn.fit(dataset)
