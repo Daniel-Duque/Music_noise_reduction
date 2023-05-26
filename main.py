bc3fdbcaaf6e523989db54c5b05796507804c397
Daniel-Duque <dduque80@hotmail.com>
2023-05-25 19:12:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import tensorflow as tf
 import os
 import tensorflow_io as tfio
 from IPython.display import Audio
-from scipy.io.wavfile import read
+from scipy.io.wavfile import read,write
 import numpy as np
 #preprocess functions
 
 
 
 general_path=r"C:/Users/usuario/Documents/GitHub/Music_noise_reduction/data/musicnet/musicnet/train_data"
+
 time=16000
 sizes=1000
 
@@ -27,12 +28,14 @@ for i in os.listdir(general_path):
     else:
         padsong=npsong[:sizes]
     mean=-2.74658203125e-07
-    padsongnorm=padsong/mean
+    padsongnorm=padsong
     dataset=np.vstack((dataset,padsongnorm))
 
 model_rn = tf.keras.Sequential()
-model_rn.add(tf.keras.layers.LSTM(8, input_shape=(sizes,1,), return_sequences=True))
+model_rn.add(tf.keras.layers.LSTM(100, input_shape=(sizes,1,), return_sequences=True))
 model_rn.add(tf.keras.layers.LSTM(1, return_sequences=True))
+opti=tf.keras.optimizers.Adam(learning_rate=0.00001)
+model_rn.compile(loss="MSE",optimizer=opti)
+model_rn.fit(dataset,dataset,epochs=10,validation_split=0.2)
+fitted=model_rn.predict(dataset)
 
-model_rn.compile(loss="MSE")
-model_rn.fit(dataset,dataset,epochs=1000)
